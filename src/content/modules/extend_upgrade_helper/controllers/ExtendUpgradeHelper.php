d5d2bd5953111f21ddacd8ea8cf8f1f1d4adee93
Der Uli im Netz <admin@deruli.de>
2020-06-21 22:53:01 +0200

    extend upgrade helper

--- src/content/modules/extend_upgrade_helper/controllers/ExtendUpgradeHelper.php
+++ src/content/modules/extend_upgrade_helper/controllers/ExtendUpgradeHelper.php
@@ -1,11 +1,13 @@
 <?php
 
-class ExtendUpgradeHelper extends Controller
-{
+declare(strict_types=1);
 
-    public function getModules()
-    {
-        $result = array();
+use UliCMS\Services\Connectors\eXtend\AvailablePackageVersionMatcher;
+
+class ExtendUpgradeHelper extends Controller {
+
+    public function getModules(): array {
+        $modulesFromExtend = [];
         $modules = getAllModules();
         foreach ($modules as $module) {
             if (getModuleMeta($module, "source") == "extend") {
@@ -13,9 +15,37 @@ class ExtendUpgradeHelper extends Controller
                 $xtendModule->name = $module;
                 $xtendModule->version = getModuleMeta($module, "version");
                 $xtendModule->url = "https://extend.ulicms.de/" . $module . ".html";
-                $result[] = $xtendModule;
+                $xtendModule->updateAvailable = $this->checkForUpdates(
+                        $xtendModule->name, $xtendModule->version
+                );
+
+                $modulesFromExtend[] = $xtendModule;
             }
         }
-        return $result;
+        return $modulesFromExtend;
     }
+
+    protected function checkForUpdates(string $name, ?string $version): bool {
+        if (!$version) {
+            return false;
+        }
+
+        $url = "https://extend.ulicms.de/{$name}.json";
+        $json = file_get_contents_wrapper($url, true);
+        if (!$json) {
+            return false;
+        }
+        $data = json_decode($json, true);
+        if (!($data and isset($data["data"]))) {
+            return false;
+        }
+        $versionMatcher = new AvailablePackageVersionMatcher($data["data"]);
+        $available = $versionMatcher->getCompatibleVersions();
+        
+        return (
+                count($available) and
+                \UliCMS\Utils\VersionComparison\compare($available[0]["version"], $version, ">")
+                );
+    }
+
 }
